f600c4193961735207821b43fd05605a886eff2b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-02-25 20:02:34 +0700

    Add external module for operations with triangles

---
+++ src/libgeometry/triangle.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "point.h"
+
+float calculateSide(struct Point point1, struct Point point2);
+float calculateTrianglePerimeter(struct Point points[4]);
+float calculateTriangleArea(struct Point points[4]);
