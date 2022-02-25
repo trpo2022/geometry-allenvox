97891d735069a41f09b6a0a124f6c096e30eae19
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-02-25 11:01:06 +0700

    Add basic user input

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello, World!");
+    char prefix[] = "(geometry) ";
+    char input[64];
+
+    printf("%s", prefix);
+    scanf("%[^\n]", &input);
+
+    char delims[] = "( ,)";
+    char* token = strtok(input, delims);
+
     return 0;
 }
